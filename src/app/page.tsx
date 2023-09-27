1919cde98b30dff5a5ef9445fd13f4a0215357ee
Cheulong Sear <searcheulong@gmail.com>
2023-09-28 02:11:35 +0700

    add cv text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-[700px] bg-slate-600 m-auto">
+      <div className="text-black bg-white">
+        <h1>Cheulong Sear</h1>
+        <div>
+          <p>cheulongsear.dev</p>
+          <p>LinkedIn</p>
+          <p>(+66) 99 220 3903</p>
+          <p>searcheulong@gmail.com</p>
+          <p>Github</p>
         </div>
-      </div>
+        <h2>Skills</h2>
+        <ul>
+          <li>
+            Typescript | Node | Express | React | NextJs | NoSQL | Tailwind css
+            | D3.js | A11y | Git
+          </li>
+          <li>
+            Azure | Swagger | Jest | Playwright | Firebase | Google Analytics |
+            Google Tag Manager
+          </li>
+          <li>
+            Frontend | Full-stack | English, Thai, Khmer - All professional
+            proficiency or above
+          </li>
+        </ul>
+        <h2>Certifications</h2>
+        <ul>
+          <li>
+            <p>Microsoft Azure Administrator Associate</p>
+            <p>Stripe Certified Professional Developer</p>
+            <p>Google UX Design Certificate</p>
+            <p>Mixpanel Partner Certification</p>
+          </li>
+        </ul>
+        <h2>Experience</h2>
+        <div>
+          <div>Seven Peaks Software – Frontend Developer Bangkok, Thailand</div>
+          <div>Feb 2022 - Present</div>
+        </div>
+        <ul>
+          <li>
+            Implemented the enterprise-level website of the hospital, connecting
+            thousands of patients and doctors every day using the latest
+            technologies of Azure, Azure Functions and Azure AD.
+          </li>
+          <li>
+            Developed the video call website of the house insurance company,
+            helping them arrange the meetings with clients smoothly, leading to
+            gaining revenue using Azure Communication, SignalR and MS Graph.
+          </li>
+          <li>
+            Designed and implemented the scalable ordering pizza website, and
+            optimized the load speed with the latest technologies using Nextjs,
+            Storybook and Playwright.
+          </li>
+          <li>
+            Continuous Integration/Deployment Pipeline Integration, pull
+            requests, code reviews, load/stress testing, and
+            unit/integration/e2e testing to improve the development speed.
+          </li>
+        </ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <div>Ling. – Full-Stack Developer Chiang Mai, Thailand</div>
+          <div>Aug 2019 - Jan 2022</div>
+        </div>
+        <ul>
+          <li>
+            Developing and maintaining the frontend functionality of websites
+            integration with social media logins, payment processors, and other
+            services.
+          </li>
+          <li>
+            Developing and maintaining the mobile apps projects on both Android
+            and IOS.
+          </li>
+          <li>
+            Developing and maintaining the firebase and firebase cloud function.
+          </li>
+          <li>
+            Add web analytics service like google analytics and facebook pixel
+            to the website.
+          </li>
+          <li>Maintain and improve database.</li>
+        </ul>
+        <div>
+          <div>Buzzwoo! Asia – Frontend Intern Chiang Mai, Thailand</div>
+          <div>Sep 2018 - May 2019</div>
+        </div>
+        <ul>
+          <li>
+            Developing high quality, high performance and highly reliable web
+            applications.
+          </li>
+        </ul>
+        <h2>Education</h2>
+        <div>Chiang Mai University: 2015 - 2019 Software Engineering</div>
       </div>
     </main>
-  )
+  );
 }
